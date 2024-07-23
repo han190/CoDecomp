da6f208aac3f44bc5c30b9e466e2da0787c0861e
han190 <limpid.mazarine@gmail.com>
2024-07-23 12:16:31 -0800

    refactor reallocate subroutines.

--- src/submodule_reallocate.f90
+++ src/submodule_reallocate.f90
@@ -7,12 +7,10 @@ pure module subroutine reallocate_int32_1d(array, new_size)
   integer, allocatable, intent(inout) :: array(:)
   integer, intent(in) :: new_size
 
-  if (allocated(array)) then
-    if (size(array) /= new_size) then
-      deallocate (array)
-      allocate (array(new_size))
-    end if
-  else
+  if (.not. allocated(array)) then
+    allocate (array(new_size))
+  else if (size(array) /= new_size) then
+    deallocate (array)
     allocate (array(new_size))
   end if
 end subroutine reallocate_int32_1d
@@ -22,63 +20,55 @@ pure module subroutine reallocate_int64_1d(array, new_size)
   integer(int64), allocatable, intent(inout) :: array(:)
   integer(int64), intent(in) :: new_size
 
-  if (allocated(array)) then
-    if (size(array) /= new_size) then
-      deallocate (array)
-      allocate (array(new_size))
-    end if
-  else
+  if (.not. allocated(array)) then
+    allocate (array(new_size))
+  else if (size(array) /= new_size) then
+    deallocate (array)
     allocate (array(new_size))
   end if
 end subroutine reallocate_int64_1d
 
 !> Reallocate 2D real array.
 pure module subroutine reallocate_real32_2d(array, new_size)
-  real, allocatable, intent(inout) :: array(:, :)
+  real, allocatable, intent(inout) :: array(:,:)
   integer, intent(in) :: new_size(:)
 
   if (rank(array) /= size(new_size)) &
     & error stop "[reallocate_real32_2d] Invalid shape."
-  if (allocated(array)) then
-    if (.not. all(shape(array) == new_size)) then
-      deallocate (array)
-      allocate (array(new_size(1), new_size(2)))
-    end if
-  else
+  if (.not. allocated(array)) then
+    allocate (array(new_size(1), new_size(2)))
+  else if (any(shape(array) /= new_size)) then
+    deallocate (array)
     allocate (array(new_size(1), new_size(2)))
   end if
 end subroutine reallocate_real32_2d
 
 !> Reallocate 3D real array.
 pure module subroutine reallocate_real32_3d(array, new_size)
-  real, allocatable, intent(inout) :: array(:, :, :)
+  real, allocatable, intent(inout) :: array(:,:,:)
   integer, intent(in) :: new_size(:)
 
   if (rank(array) /= size(new_size)) &
     & error stop "[reallocate_real32_3d] Invalid shape."
-  if (allocated(array)) then
-    if (.not. all(shape(array) == new_size)) then
-      deallocate (array)
-      allocate (array(new_size(1), new_size(2), new_size(3)))
-    end if
-  else
+  if (.not. allocated(array)) then
+    allocate (array(new_size(1), new_size(2), new_size(3)))
+  else if (any(shape(array) /= new_size)) then
+    deallocate (array)
     allocate (array(new_size(1), new_size(2), new_size(3)))
   end if
 end subroutine reallocate_real32_3d
 
 !> Reallocate 3D complex array.
 pure module subroutine reallocate_complex32_3d(array, new_size)
-  complex, allocatable, intent(inout) :: array(:, :, :)
+  complex, allocatable, intent(inout) :: array(:,:,:)
   integer, intent(in) :: new_size(:)
 
   if (rank(array) /= size(new_size)) &
     & error stop "[reallocate_complex32_3d] Invalid shape."
-  if (allocated(array)) then
-    if (.not. all(shape(array) == new_size)) then
-      deallocate (array)
-      allocate (array(new_size(1), new_size(2), new_size(3)))
-    end if
-  else
+  if (.not. allocated(array)) then
+    allocate (array(new_size(1), new_size(2), new_size(3)))
+  else if (any(shape(array) /= new_size)) then
+    deallocate (array)
     allocate (array(new_size(1), new_size(2), new_size(3)))
   end if
 end subroutine reallocate_complex32_3d
